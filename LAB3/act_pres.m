34db4d1a672117b375f509d8fedd8d906d1573c9
Mair <163215578+MaiR42@users.noreply.github.com>
2026-05-17 14:06:06 -0400

    Clean up comments and plots in act_pres.m
    Removed comments and plots related to FSK bandwidth and the real part of the complex envelope.

---
+++ LAB3/act_pres.m
@@ -0,0 +1,56 @@
+close all;
+% % % Envolvente compleja
+
+% Dado a que La FSK es idéntica a la modulación de una portadora de
+% FM mediante una señal digital binaria:
+
+% g(t) = Ac * exp(j2πΔf∫m(t)dt)
+% Ac: Amplitud de la portadora ; m(t): Señal binaria
+
+% Bits
+bits = [1 0 1 1 0 0 1 0];
+
+% Parametros
+Ac = 1;
+Rb = 1e3;
+Tb = 1/Rb;
+Fs = 100*Rb;
+
+
+fdev = 2e3;   % desviacion de frecuencia (Δf)
+
+Ns = round(Fs/Rb);
+
+% Señal polar
+m = repelem(2*bits-1, Ns);
+
+t = (0:length(m)-1)/Fs;
+
+% Integral discreta de m(t)
+phi = 2*pi*fdev*cumsum(m)/Fs;
+
+% Envolvente compleja
+g = Ac * exp(1j*phi);
+
+% FFT
+Nfft = 2^nextpow2(length(g));
+
+G = fftshift(fft(g,Nfft))/length(g);
+
+f = (-Nfft/2:Nfft/2-1)*(Fs/Nfft);
+
+% Graficas
+figure;
+
+subplot(3,1,1)
+plot(t,m)
+grid on
+title('Señal binaria polar')
+xlabel('Tiempo [s]')
+
+subplot(3,1,3)
+plot(f,abs(G))
+grid on
+title('Transformada de Fourier de g(t)')
+xlabel('Frecuencia [Hz]')
+xlim([-10e3 10e3])
